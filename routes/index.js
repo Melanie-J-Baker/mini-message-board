b11106734787aa691a2514a5b3b29f6bd53416c1
Melanie-J-Baker <baker_mel@hotmail.com>
2024-05-16 11:36:30 +0100

    Add ESLint

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Message Board", messages: messages });
-});
-
-router.get("/new", function (req, res, next) {
-  res.render("form");
-});
-
-router.post("/new", function (req, res, next) {
-  const message = req.body;
-  console.log(message);
-  messages.push({ text: message.text, user: message.user, added: new Date() });
-  res.redirect("/");
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
